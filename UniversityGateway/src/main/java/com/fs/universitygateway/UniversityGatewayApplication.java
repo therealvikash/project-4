07a17aff81e9f5473c17c52b4cf96b83bcffa5e4
bikumary <bikumary@LIN60017688.corp.capgemini.com>
2022-02-15 12:55:54 +0530

    First Commit

---
+++ UniversityGateway/src/main/java/com/fs/universitygateway/UniversityGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.fs.universitygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityGatewayApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(UniversityGatewayApplication.class, args);
+	}
+
+}
---
+++ UniversityGateway/src/test/java/com/fs/universitygateway/UniversityGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.universitygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
